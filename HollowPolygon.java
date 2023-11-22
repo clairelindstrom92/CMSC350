3a121e7c88ee304f86a43e82baaeff5fdf7a837f
Claire Lindstrom <90209948+clairelindstrom92@users.noreply.github.com>
2023-11-21 19:03:03 -0500

    Add files via upload

---
+++ HollowPolygon.java
@@ -0,0 +1,25 @@
+// CMSC 330 Advanced Programming Languages
+// Project 1 Skeleton
+// UMGC CITE
+// Claire Lindstrom
+// October 2023
+
+import java.awt.*;
+
+// Class that defines all hollow polygons
+
+class HollowPolygon extends Polygon_ {
+
+    // Constructor that calls super constructor
+
+    public HollowPolygon(Color color, int vertexCount) {
+        super(color, vertexCount);
+    }
+
+    // Draws hollow polygon
+
+    @Override
+    public void drawPolygon(Graphics graphics, Polygon polygon) {
+        graphics.drawPolygon(polygon);
+    }
+}
---
+++ Token.java
@@ -0,0 +1,12 @@
+// CMSC 330 Advanced Programming Languages
+// Project 1 Skeleton
+// UMGC CITE
+//Claire Lindstrom
+//November 2023
+// Enumerated type that defines the list of tokens
+
+enum Token {
+    AT, COLOR, END, HEIGHT, RECTANGLE, RIGHT_TRIANGLE, SCENE, WIDTH, 
+    COMMA, SEMICOLON, PERIOD, LEFT_PAREN, RIGHT_PAREN, IDENTIFIER, NUMBER, EOF,
+    PARALLELOGRAM, REGULAR_POLYGON, ISOSCELES, TEXT, SIDES, RADIUS, OFFSET, STRING
+}
